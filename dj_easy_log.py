091fadf45b951450d5fbd56177635571fab400ef
Paul Bailey <paul@neutron.studio>
2023-03-07 13:09:33 -0600

    update settings

--- dj_easy_log.py
+++ dj_easy_log.py
@@ -48,7 +48,7 @@ class InterceptHandler(logging.Handler):
       frame = frame.f_back
       depth += 1
 
-    logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
+    logger.opt(depth=depth, exception=record.exc_info, diagnose=False).log(level, record.getMessage())
 
 
 def load_loguru(settings, configure_func=None, logging_config=None, loglevel=None):
